f9a57532bc042dfbe51eed5fc5dd3c2dc8a3409b
Simon-ux23 <semeontarasenko@yandex.ru>
2021-06-26 10:17:41 +0300

    Добавил файлы из раздела ввод и вывод данных

---
+++ num_6.py
@@ -0,0 +1,4 @@
+#Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!).
+n = int(input("введите число"))
+print("The previous number for the number", str(n), "is", str(n - 1))
+print("The next number for the number", str(n), "is", str(n + 1))

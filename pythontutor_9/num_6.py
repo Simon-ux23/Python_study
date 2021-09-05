4273b469a0939e493b3ff1e63715dc8cb78d00ef
Simon-ux23 <semeontarasenko@yandex.ru>
2021-09-05 17:59:10 +0300

    сделал , надо быть внимательнее

--- pythontutor_9/num_6.py
+++ pythontutor_9/num_6.py
@@ -3,5 +3,16 @@
 n, m = [int(i) for i in input().split()]
 a = [[int(j) for j in input().split()]for i in range(n)]
 i, j = [int(i) for i in input().split()]
+f = a[0][0]
 for s in range(n):
     for k in range(m):
+        if k == i:
+            f = a[s][i]
+            a[s][i] = a[s][j]
+        if k == j:
+            a[s][j] = f
+
+for i in a:
+    for j in i:
+        print(int(j), end = " ")
+    print()
